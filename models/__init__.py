fde805dabecb0e46fff6633784bc62405ec77544
Justineneema <j.neema@alustudent.com>
2025-05-24 21:41:55 +0200

    Cloned repository

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
-
-from os import getenv
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
+import os
 
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
+# storage = FileStorage()
+# storage.reload()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
+
